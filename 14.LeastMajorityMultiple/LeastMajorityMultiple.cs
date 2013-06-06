9ad58ddb89fa73eb99ab438c3981ea2119d30077
Nikolay Nikolov <nikolov89@gmail.com>
2013-06-06 18:57:41 +0300

    added some resolved exam tasks

---
+++ 14.LeastMajorityMultiple/LeastMajorityMultiple.cs
@@ -0,0 +1,70 @@
+﻿using System;
+
+class LeastMajorityMultiple
+{
+    static void Main()
+    {
+        int[] numbers = new int[5];
+        int count = 0;
+        long biggestNumber = 0;
+        long result = 0;
+        long allCombinations = 10000000000;
+
+        for (int i = 0; i < numbers.Length; i++)
+        {
+            numbers[i] = int.Parse(Console.ReadLine());
+        }
+
+        // determin the biggest number
+        for (int i = 0; i < numbers.Length - 1; i++)
+        {
+            if (numbers[i] < numbers[i + 1])
+            {
+                biggestNumber = numbers[i + 1];
+            }
+            if (biggestNumber == 0) biggestNumber = numbers[0];
+        }
+
+        // Lest Majourity Multilple is part of the variables
+        if (count == 0)
+        {
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                count = 0;
+                for (int j = 0; j < numbers.Length; j++)
+                {
+
+                    if (numbers[i] % numbers[j] == 0)
+                    {
+                        count++;
+                    }
+                }
+                if (count >= 3)
+                {
+                    Console.WriteLine(numbers[i]);
+                    break;
+                }
+            }
+        }
+        if (count < 3)
+        {
+            for (long k = biggestNumber + 1; k < allCombinations; k++)
+            {
+                count = 0;
+                for (int p = 0; p < numbers.Length; p++)
+                {
+                    if (k % numbers[p] == 0)
+                    {
+                        count++;
+                    }
+                }
+                if (count >= 3)
+                {
+                    result = k;
+                    break;
+                }
+            }
+            Console.WriteLine(result);
+        }
+    }
+}
---
+++ 16.OddNumber/OddNumber.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Numerics;
+
+class OddNumber
+{
+    static void Main()
+    {
+        int numbersCount = int.Parse(Console.ReadLine()); // reads the quantity of numbers to lookup for OddNumber
+
+        long oddNumber = 0;
+        long number = 0;
+        for (int i = 0; i < numbersCount; i++) // Loop to enter numbers
+        {
+            number = long.Parse(Console.ReadLine()); // reads a number from the console
+            oddNumber = oddNumber ^ number; // pumping numbers into the variable using XOR.
+            //The effect is that "x" XOR "x" = 0, and finally 0 XOR "y" = "y" nevertheless the position of y into the array  
+        }
+
+        Console.WriteLine(oddNumber);
+        
+        //int N = int.Parse(Console.ReadLine());
+        //long[] numbers = new long[N];
+
+        //for (int i = 0; i < numbers.Length; i++)
+        //{
+        //    numbers[i] = long.Parse(Console.ReadLine());
+        //}
+
+        //long count = 1;
+        //long temp = 0;
+
+        //for (int i = 0; i < numbers.Length; i++)
+        //{
+        //    count = 1;
+        //    for (int j = i; j < numbers.Length; j++)
+        //    {
+        //        if (numbers[i] == numbers[j])
+        //        {
+        //            count++;
+        //            temp = numbers[i];
+        //        }
+        //    }
+        //    if (count % 2 == 0)
+        //    {
+        //        Console.WriteLine(temp);
+        //        break;
+        //    }
+            
+        //}
+    }
+}
