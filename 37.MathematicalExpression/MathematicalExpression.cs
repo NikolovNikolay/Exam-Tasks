8f0116a57595f3a5a779ea6dfb7c538d2a5467e5
Nikolay Nikolov <nikolov89@gmail.com>
2013-06-23 12:50:57 +0300

    new tasks

---
+++ 37.MathematicalExpression/MathematicalExpression.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        double N = double.Parse(Console.ReadLine());
+        double M = double.Parse(Console.ReadLine());
+        double P = double.Parse(Console.ReadLine());
+
+        const double number = 1337;
+        const double numberInDenominator = 128.523123123;
+
+        double up = N * N + (1 / (M * P)) + number;
+        double down = N - numberInDenominator * P;
+
+        double result = up / down + Math.Sin((int)(M % 180));
+        Console.WriteLine("{0:F6}", result);
+    }
+}
