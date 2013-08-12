f21c489d72dfa3fa24ce4cf85342fb45d16cf425
Nikolay Nikolov <nikolov89@gmail.com>
2013-08-12 19:06:45 +0300

    Tasks added

--- C# Part 2 Exam tasks/02.GreedyDwarf/GreedyDwarf.cs
+++ C# Part 2 Exam tasks/02.GreedyDwarf/GreedyDwarf.cs
@@ -1,68 +1,142 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
 
-class GreedyDwarf
+//class GreedyDwarf
+//{
+//    static void Main()
+//    {
+//        string[] numbers = Console.ReadLine().Split(new char[] { ',', ' ' }, StringSplitOptions.RemoveEmptyEntries);
+
+//        int[] valley = new int[numbers.Length];
+//        for (int i = 0; i < numbers.Length; i++)
+//        {
+//            valley[i] = int.Parse(numbers[i]);
+//        }
+
+//        int m = int.Parse(Console.ReadLine());
+//        long maxCoins = long.MinValue;
+//        for (int i = 0; i < m; i++)
+//        {
+//            long sum = SumCoinsInPattern(valley);
+
+//            if (sum > maxCoins)
+//            {
+//                maxCoins = sum;
+//            }
+//        }
+
+//        Console.WriteLine(maxCoins);
+//    }
+
+//    private static long SumCoinsInPattern(int[] valley)
+//    {
+//        string[] numbers = Console.ReadLine().Split(new char[] { ',', ' ' }, StringSplitOptions.RemoveEmptyEntries);
+//        int[] pattern = new int[numbers.Length];
+//        for (int i = 0; i < pattern.Length; i++)
+//        {
+//            pattern[i] = int.Parse(numbers[i]);
+//        }
+
+//        long currentCoins = valley[0];
+//        int currentPosition = 0;
+//        bool[] visited = new bool[valley.Length];
+//        visited[currentPosition] = true;
+
+//        while (true)
+//        {
+//            for (int i = 0; i < pattern.Length; i++)
+//            {
+//                int nextMove = currentPosition + pattern[i];
+
+//                if ((nextMove >= 0 && nextMove < valley.Length) && !visited[nextMove])
+//                {
+//                    currentCoins += valley[nextMove];
+//                    visited[nextMove] = true;
+//                    currentPosition = nextMove;
+//                }
+//                else
+//                {
+//                    return currentCoins;
+//                }
+//            }
+//        }
+
+//    }
+//}
+
+using System;
+namespace GreedyDwarf
 {
-    static void Main()
+    class GreedyDwarf
     {
-        string[] numbers = Console.ReadLine().Split(new char[] { ',', ' ' }, StringSplitOptions.RemoveEmptyEntries);
-
-        int[] valley = new int[numbers.Length];
-        for (int i = 0; i < numbers.Length; i++)
+        private static long ProccessPattern(int[] Vally)
         {
-            valley[i] = int.Parse(numbers[i]);
-        }
 
-        int m = int.Parse(Console.ReadLine());
-        long maxCoins = long.MinValue;
-        for (int i = 0; i < m; i++)
-        {
-            long sum = SumCoinsInPattern(valley);
+            string[] rawNumbers = Console.ReadLine().Split(new char[] { ' ', ',' }, StringSplitOptions.RemoveEmptyEntries);
 
-            if (sum > maxCoins)
-            {
-                maxCoins = sum;
-            }
-        }
-
-        Console.WriteLine(maxCoins);
-    }
-
-    private static long SumCoinsInPattern(int[] valley)
-    {
-        string[] numbers = Console.ReadLine().Split(new char[] { ',', ' ' }, StringSplitOptions.RemoveEmptyEntries);
-        int[] pattern = new int[numbers.Length];
-        for (int i = 0; i < pattern.Length; i++)
-        {
-            pattern[i] = int.Parse(numbers[i]);
-        }
-
-        long currentCoins = valley[0];
-        int currentPosition = 0;
-        bool[] visited = new bool[valley.Length];
-        visited[currentPosition] = true;
-
-        while (true)
-        {
+            int[] pattern = new int[rawNumbers.Length];
             for (int i = 0; i < pattern.Length; i++)
             {
-                int nextMove = currentPosition + pattern[i];
+                pattern[i] = int.Parse(rawNumbers[i]);
+            }
 
-                if ((nextMove >= 0 && nextMove < valley.Length) && !visited[nextMove])
+            long coinSum = new long();
+            coinSum += Vally[0];
+
+            bool[] visited = new bool[Vally.Length];
+            visited[0] = true;
+
+            int currentPosition = 0;
+            while (true)
+            {
+                for (int i = 0; i < pattern.Length; i++)
                 {
-                    currentCoins += valley[nextMove];
-                    visited[nextMove] = true;
-                    currentPosition = nextMove;
+                    int nextMove = currentPosition + pattern[i];
+                    if ((nextMove >= 0 && nextMove < Vally.Length) && !visited[nextMove])
+                    {
+                        coinSum += Vally[nextMove];
+                        visited[nextMove] = true;
+                        currentPosition = nextMove;
+
+                    }
+                    else
+                    {
+                        return coinSum;
+                    }
+
                 }
-                else
-                {
-                    return currentCoins;
-                }
-            } 
+            }
+
         }
 
+        static void Main()
+        {
+            string[] rawNumbers = Console.ReadLine().Split(new char[] { ' ', ',' }, StringSplitOptions.RemoveEmptyEntries);
+            int[] Valley = new int[rawNumbers.Length];
+
+            for (int i = 0; i < Valley.Length; i++)
+            {
+                Valley[i] = int.Parse(rawNumbers[i]);
+            }
+
+            int numberOfPatterns = int.Parse(Console.ReadLine());
+
+            long bestSum = long.MinValue;
+            for (int i = 0; i < numberOfPatterns; i++)
+            {
+                long sum = ProccessPattern(Valley);
+                if (sum > bestSum)
+                {
+                    bestSum = sum;
+                }
+
+            }
+            Console.WriteLine(bestSum);
+
+        }
     }
+
 }
---
+++ C# Part 2 Exam tasks/Slides/Slides.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+class Program
+{
+    static string[, ,] cuboid;
+    static string[] directions = { "L", "R", "F", "B", "FL", "FR", "BL", "BR" };
+    static int[] dirsW = { -1, 1, 0, 0, -1, 1, -1, 1 };
+    static int[] dirsH = { 1, 1, 1, 1, 1, 1, 1, 1 };
+    static int[] dirsD = { 0, 0, -1, 1, -1, -1, 1, 1 };
+
+    static void Main()
+    {
+        string[] dimentions = Console.ReadLine().Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+        int W = int.Parse(dimentions[0]);
+        int H = int.Parse(dimentions[1]);
+        int D = int.Parse(dimentions[2]);
+        cuboid = new string[W, H, D];
+
+        for (int i = 0; i < H; i++)
+        {
+            string line = Console.ReadLine();
+            string[] layers = Array.ConvertAll(line.Split('|'), p => p.Trim());
+            for (int j = 0; j < D; j++)
+            {
+                string[] currentElements = layers[j].Substring(1, layers[j].Length - 2).Split(new string[] { ")(" }, StringSplitOptions.RemoveEmptyEntries);
+                for (int k = 0; k < W; k++)
+                {
+                    cuboid[k, i, j] = currentElements[k];
+                }
+            }
+        }
+
+        string[] ballCoordinates = Console.ReadLine().Split(' ');
+        int ballW = int.Parse(ballCoordinates[0]);
+        int ballD = int.Parse(ballCoordinates[1]);
+        int ballH = 0;
+
+        while (true)
+        {
+            string[] ballPosition = cuboid[ballW, ballH, ballD].Split();
+            if (ballPosition[0] == "S")
+            {
+                int index = Array.IndexOf(directions, ballPosition[1]);
+                if (IsInCuboid(ballW + dirsW[index], ballH + dirsH[index], ballD + dirsD[index]))
+                {
+                    ballW += dirsW[index];
+                    ballH += dirsH[index];
+                    ballD += dirsD[index];
+                }
+                else
+                {
+                    PrintResult(ballW, ballH, ballD);
+                    return;
+                }
+            }
+            else if (ballPosition[0] == "T")
+            {
+                ballW = int.Parse(ballPosition[1]);
+                ballD = int.Parse(ballPosition[2]);
+            }
+            else if (ballPosition[0] == "B")
+            {
+                PrintResult(ballW, ballH, ballD);
+                return;
+            }
+            else if (ballPosition[0] == "E")
+            {
+                if (ballH >= H - 1)
+                {
+                    PrintResult(ballW, ballH, ballD);
+                    return;
+                }
+                ballH++;
+            }
+        }
+    }
+
+    static bool IsInCuboid(int W, int H, int D)
+    {
+        if (W < 0 || W >= cuboid.GetLength(0) || H < 0 || H >= cuboid.GetLength(1) || D < 0 || D >= cuboid.GetLength(2))
+        {
+            return false;
+        }
+        return true;
+    }
+
+    static void PrintResult(int W, int H, int D)
+    {
+        Console.WriteLine(H == cuboid.GetLength(1) - 1 && cuboid[W, H, D] != "B" ? "Yes" : "No");
+        Console.WriteLine("{0} {1} {2}", W, H, D);
+    }
+}
