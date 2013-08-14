648f25dcc59e1eaf167d533006c17e6273eac642
Nikolay Nikolov <nikolov89@gmail.com>
2013-08-14 17:34:02 +0300

    New exam tasks added

---
+++ C# Part 2 Exam tasks/CSharpBrackets/CSharpBrackets.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+class CSharpBrackets
+{
+    static void Main()
+    {
+        //DateTime now = DateTime.Now;
+        if (File.Exists("input.txt"))
+        {
+            Console.SetIn(new StreamReader("input.txt"));
+        }
+
+        int numberOfLines = int.Parse(Console.ReadLine());
+        string inlineString = Console.ReadLine();
+        var builder = new StringBuilder();
+        int brackets = 0;
+        for (int i = 0; i < numberOfLines; i++)
+        {
+            string line = Console.ReadLine().Trim();
+            if (line == string.Empty)
+            {
+                continue;
+            }
+            if (line.IndexOf('{') == -1 && line.IndexOf('}') == -1)
+            {
+                string[] words = line.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+                for (int j = 0; j < words.Length; j++)
+                {
+                    builder.AppendFormat("{0} ", words[j]);
+
+                }
+                for (int j = 0; j < brackets; j++)
+                {
+                    builder.Insert(0, inlineString);
+                }
+                Console.WriteLine(builder.ToString().Trim());
+                builder.Clear();
+            }
+            else
+            {
+                for (int j = 0; j < line.Length; j++)
+                {
+                    char ch = line[j];
+                    if (ch == '{')
+                    {
+                        if (builder.Length > 0)
+                        {
+                            for (int k = 0; k < brackets; k++)
+                            {
+                                builder.Insert(0, inlineString);
+                            }
+                            Console.WriteLine(builder.ToString().Trim());
+                            builder.Clear();
+                        }
+                        builder.Append(ch);
+                        for (int k = 0; k < brackets; k++)
+                        {
+                            builder.Insert(0, inlineString);
+                        }
+                        Console.WriteLine(builder.ToString().Trim());
+                        brackets++;
+                        builder.Clear();
+                    }
+                    else if (ch == '}')
+                    {
+                        if (builder.Length > 0)
+                        {
+                            for (int k = 0; k < brackets; k++)
+                            {
+                                builder.Insert(0, inlineString);
+                            }
+                            Console.WriteLine(builder.ToString().Trim());
+                            builder.Clear();
+                        }
+                        builder.Append(ch);
+                        brackets--;
+                        for (int k = 0; k < brackets; k++)
+                        {
+                            builder.Insert(0, inlineString);
+                        }
+                        Console.WriteLine(builder.ToString().Trim());
+                        builder.Clear();
+                    }
+                    else if (ch == ';' && j == line.Length - 1)
+                    {
+                        builder.Append(ch);
+                        for (int k = 0; k < brackets; k++)
+                        {
+                            builder.Insert(0, inlineString);
+                        }
+                        Console.WriteLine(builder.ToString().Trim());
+                        builder.Clear();
+                    }
+                    else
+                    {
+                        if (ch == ' ' && builder.Length > 0 && builder[builder.Length - 1] != ' ')
+                        {
+                            builder.Append(ch);
+                        }
+                        else if (ch != ' ')
+                        {
+                            builder.Append(ch);
+                        }
+                    }
+                }
+            }
+           
+        }
+        //TimeSpan span = DateTime.Now - now;
+    }
+}
---
+++ C# Part 2 Exam tasks/KukataIsDancing/KukataIsDancing.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Linq;
+
+class KukataIsDancing
+{
+    public static int[,] cube = new int[3, 3];
+
+    static void Main()
+    {
+        cube[0, 0] = cube[0, 2] = cube[2, 0] = cube[2, 2] = 1; // 1 for red
+        cube[1, 1] = 2; // 2 for green
+        
+        int dances = int.Parse(Console.ReadLine());
+        int row = 1;
+        int col = 1;
+        int direction = 0; // 0 down, 1 right, 2 up, 3 left
+
+        for (int dance = 0; dance < dances; dance++)
+        {
+            row = 1;
+            col = 1;
+            direction = 0;
+            string current = Console.ReadLine();
+            for (int ch = 0; ch < current.Length; ch++)
+            {
+                char move = current[ch];
+                if (move == 'L')
+                {
+                    direction++;
+                    if (direction > 3)
+                    {
+                        direction = 0;
+                    }
+                }
+                else if (move == 'R')
+                {
+                    direction--;
+                    if (direction < 0)
+                    {
+                        direction = 3;
+                    }
+                }
+                else
+                {
+                    if (direction == 0)
+                    {
+                        row = row + 1;
+                        row = CheckBoundariesRow(row);
+                    }
+                    else if (direction == 1)
+                    {
+                        col = col + 1;
+                        col = CheckBoundariesCol(col);
+                    }
+                    else if (direction == 2)
+                    {
+                        row = row - 1;
+                        row = CheckBoundariesRow(row);
+                    }
+                    else if (direction == 3)
+                    {
+                        col = col - 1;
+                        col = CheckBoundariesCol(col);
+                    }
+                }
+
+            }
+            if (cube[row, col] == 1)
+            {
+                Console.WriteLine("RED");
+            }
+            else if (cube[row, col] == 2)
+            {
+                Console.WriteLine("GREEN");
+            }
+            else
+            {
+                Console.WriteLine("BLUE");
+            }
+        }
+    }
+
+    static int CheckBoundariesRow(int row)
+    {
+        
+        if (row == -1)
+        {
+            row = 2;
+            return row;
+        }
+        else if (row == 3)
+        {
+            row = 0;
+            return row;
+        }
+        
+        return row;
+    }
+
+    static int CheckBoundariesCol(int col)
+    {
+        if (col == -1)
+        {
+            col = 2;
+            return col;
+        }
+        else if (col == 3)
+        {
+            col = 0;
+            return col;
+        }
+        return col;
+    }
+}
+        
